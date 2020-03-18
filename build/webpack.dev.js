2e6d967cac98beaee7e964b52f7a111df635a91d
Guillaume Briday <guillaumebriday@gmail.com>
2020-03-18 12:21:37 +0100

    Adding options to webpack-dev-server

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -9,12 +9,18 @@ module.exports = merge(common, {
     filename: 'js/[name]-[hash].js'
   },
   devServer: {
+    https: false,
     historyApiFallback: true,
     noInfo: true,
     overlay: true,
     host: 'localhost',
     port: 8080,
     hot: true,
+    compress: true,
+    inline: true,
+    contentBase: 'src',
+    watchContentBase: true,
+    open: true,
     before: function () {
       console.log('[wds]: Project is running at http://localhost:8080')
     }
